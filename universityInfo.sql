7653178aa485f960e68ee635949a5a8c53bb2f01
Saaid Faraz <ac117413@avcol.school.nz>
2023-04-04 13:27:48 +1200

    Uni schema and university info table created.

---
+++ universityInfo.sql
@@ -0,0 +1,11 @@
+﻿CREATE SCHEMA uni
+GO
+
+CREATE TABLE uni.universityInfo (
+	[universityID] INT IDENTITY (1,1) PRIMARY KEY,
+	[name] VARCHAR (255) NOT NULL,
+	[city] VARCHAR (255) NOT NULL,
+	[rankingNZ] INT NOT NULL,
+	[rankingGlobal] INT NOT NULL
+	)
+
