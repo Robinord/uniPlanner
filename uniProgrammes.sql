74cf79f61790524dd9c09557dc829fa99829410a
Saaid Faraz <ac117413@avcol.school.nz>
2023-04-04 14:15:21 +1200

    Created the UniProgramme Junction table

---
+++ uniProgrammes.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE uni.uniProgrmammes (
+	[uniProgrammeID] INT IDENTITY (1,1) PRIMARY KEY,
+	[programmeID] INT NOT NULL,
+	[universityID] INT NOT NULL,
+	[link] VARCHAR (255) NOT NULL
+	)
