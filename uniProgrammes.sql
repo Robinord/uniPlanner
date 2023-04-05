c154dda7fd745945959a9225ff8bd02c62335f7a
Saaid Faraz <ac117413@avcol.school.nz>
2023-04-06 10:41:52 +1200

    Corrected uniProgramme table name.

--- uniProgrammes.sql
+++ uniProgrammes.sql
@@ -1,4 +1,4 @@
-﻿CREATE TABLE uni.uniProgrmammes (
+﻿CREATE TABLE uni.uniProgrammes (
 	[uniProgrammeID] INT IDENTITY (1,1) PRIMARY KEY,
 	[programmeID] INT NOT NULL REFERENCES uni.programmes(programmeID),
 	[universityID] INT NOT NULL REFERENCES uni.universityInfo(universityID),
