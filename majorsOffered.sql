6376dcf50c69a7d274082caba32a6173ddcce593
Saaid Faraz <ac117413@avcol.school.nz>
2023-04-06 10:43:44 +1200

    Created the majorsOffered table.

---
+++ majorsOffered.sql
@@ -0,0 +1,5 @@
+﻿CREATE TABLE uni.majorsOffered(
+	[majorOfferedID] INT IDENTITY (1,1) PRIMARY KEY,
+	[uniProgrammeID] INT NOT NULL REFERENCES uni.uniProgrammes(uniProgrammeID),
+	[majorID] INT NOT NULL REFERENCES uni.majors(majorID),
+)
