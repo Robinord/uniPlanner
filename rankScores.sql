9b6ef01c80eaaacc71204623a2d3759cf192d01c
Saaid Faraz <ac117413@avcol.school.nz>
2023-04-06 10:45:49 +1200

    Created the rankScores table.

---
+++ rankScores.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE uni.rankScores(
+	[rankScoreID] INT IDENTITY (1,1) PRIMARY KEY,
+	[uniProgrammeID] INT NOT NULL REFERENCES uni.uniProgrammes(uniProgrammeID),
+	[rankScore] INT NOT NULL 
+	)
+
