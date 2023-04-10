5ced6cef39111f771fd62d08cd1f19283a9f6be4
Saaid Faraz <farazsaaid@gmail.com>
2023-04-10 15:30:33 +1200

    Created the grades table.

---
+++ grades.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [secondary].[grades](
+	[gradeID] INT IDENTITY (1,1) PRIMARY KEY,
+	[studentEmail] VARCHAR(255) NOT NULL REFERENCES [secondary].students(email),
+	[AS] INT NOT NULL REFERENCES [secondary].approvedAS([AS]),
+	[grade] CHAR(1) NOT NULL CHECK (grade='E' OR grade='M' OR grade='A')
+	)
