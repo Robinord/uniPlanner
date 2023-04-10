a2d43557d1df893937c9ff2ba1d0d98b7f54bbe6
Saaid Faraz <farazsaaid@gmail.com>
2023-04-10 15:27:55 +1200

    Created programmeReq table.

---
+++ programmeReq.sql
@@ -0,0 +1,5 @@
+﻿CREATE TABLE [uni].[programmeReq](
+	[programmeReqID] INT IDENTITY (1,1) PRIMARY KEY,
+	[uniProgrammeID] INT NOT NULL REFERENCES uni.uniProgrammes(uniProgrammeID),
+	[AS] INT NOT NULL REFERENCES [secondary].approvedAS([AS])
+	)
