b9716cc0644f1b15bc707743588e1ddf34e970df
Saaid Faraz <farazsaaid@gmail.com>
2023-04-11 22:09:26 +1200

    Dropped unnecessary type field in approvedAS table.

--- approvedAS.sql
+++ approvedAS.sql
@@ -4,6 +4,5 @@ GO
 CREATE TABLE [secondary].[approvedAS](
 	[AS] INT PRIMARY KEY,
 	[subject] VARCHAR (255) NOT NULL,
-	[credits] INT NOT NULL,
-	[type] VARCHAR (255) NOT NULL CHECK(type='Internal' OR type='External')
+	[credits] INT NOT NULL
 	)
