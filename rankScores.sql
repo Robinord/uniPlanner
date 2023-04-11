958d9d85927ddf95da3729810811b470a10e5889
Saaid Faraz <farazsaaid@gmail.com>
2023-04-11 22:07:09 +1200

    Added scheck constraint for rankScore.

--- rankScores.sql
+++ rankScores.sql
@@ -1,6 +1,6 @@
 ﻿CREATE TABLE [uni].[rankScores](
 	[rankScoreID] INT IDENTITY (1,1) PRIMARY KEY,
 	[uniProgrammeID] INT NOT NULL REFERENCES [uni].[uniProgrammes]([uniProgrammeID]),
-	[rankScore] INT NOT NULL 
+	[rankScore] INT NOT NULL CHECK(rankScore >=0 AND rankScore <=320)
 	)
 
