6ccb0ff6d54cc162434195ae35639e6f1c764e1a
Saaid Faraz <farazsaaid@gmail.com>
2023-04-21 14:43:47 +1200

    Took out ranking for universities.

--- universityInfo.sql
+++ universityInfo.sql
@@ -4,8 +4,6 @@ GO
 CREATE TABLE [uni].[universityInfo] (
 	[universityID] INT IDENTITY (1,1) PRIMARY KEY,
 	[name] NVARCHAR (255) NOT NULL,
-	[city] NVARCHAR (255) NOT NULL,
-	[rankingNZ] INT NOT NULL,
-	[rankingGlobal] INT NOT NULL
+	[city] NVARCHAR (255) NOT NULL
 	)
 
