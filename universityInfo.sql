ca1b937b53aece631e50f85063a480ccb1033b0d
Saaid Faraz <farazsaaid@gmail.com>
2023-04-21 14:52:11 +1200

    changed city field to location in university info.

--- universityInfo.sql
+++ universityInfo.sql
@@ -4,6 +4,6 @@ GO
 CREATE TABLE [uni].[universityInfo] (
 	[universityID] INT IDENTITY (1,1) PRIMARY KEY,
 	[name] NVARCHAR (255) NOT NULL,
-	[city] NVARCHAR (255) NOT NULL
+	[location] NVARCHAR (255) NOT NULL
 	)
 
