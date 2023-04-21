3d2201acaabb9161e70a79374e809183e3c48a9d
Saaid Faraz <farazsaaid@gmail.com>
2023-04-21 15:15:43 +1200

    seperated location field and added different fields for different ranking systems.

--- universityInfo.sql
+++ universityInfo.sql
@@ -4,6 +4,10 @@ GO
 CREATE TABLE [uni].[universityInfo] (
 	[universityID] INT IDENTITY (1,1) PRIMARY KEY,
 	[name] NVARCHAR (255) NOT NULL,
-	[location] NVARCHAR (255) NOT NULL
+	[city] NVARCHAR (255) NOT NULL,
+	[region] NVARCHAR(255) NOT NULL,
+	[THE rank] INT NOT NULL,
+	[QS rank] INT NOT NULL,
+	[ARWU rank] INT NOT NULL
 	)
 
