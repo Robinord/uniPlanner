6298fa3e9aecbad90b979183fec41ce2952382ca
Saaid Faraz <farazsaaid@gmail.com>
2023-04-23 23:42:16 +1200

    took out type field

--- Programmes.sql
+++ Programmes.sql
@@ -1,5 +1,4 @@
 ﻿CREATE TABLE [uni].[programmes] (
 	[programmeID] INT IDENTITY (1,1) PRIMARY KEY,
-	[name] NVARCHAR (255) NOT NULL,
-	[type] NVARCHAR (255) NOT NULL CHECK (type = 'Bachelor (Honours)' OR type = 'Bachelor' OR type = 'Diploma' OR type = 'Certificate')  
+	[name] NVARCHAR (255) NOT NULL
 	)
