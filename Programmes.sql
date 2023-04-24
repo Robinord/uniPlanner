4298d406f80e8aac7321dc8f319093338175e6e9
Saaid Faraz <farazsaaid@gmail.com>
2023-04-24 15:34:05 +1200

    Added rank score field to programmes table.

--- Programmes.sql
+++ Programmes.sql
@@ -1,4 +1,5 @@
 ﻿CREATE TABLE [uni].[programmes] (
 	[programmeID] INT IDENTITY (1,1) PRIMARY KEY,
-	[name] NVARCHAR (255) NOT NULL
+	[name] NVARCHAR (255) NOT NULL,
+	[rankScore] INT NULL CHECK(rankScore >=0 AND rankScore <=320) DEFAULT NULL
 	)
