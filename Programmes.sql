e93a679b69cfd9e4e7b9842051b248856020dd81
Saaid Faraz <farazsaaid@gmail.com>
2023-05-01 06:55:13 +1200

    Took out rank score field from the programmes table.

--- Programmes.sql
+++ Programmes.sql
@@ -1,5 +1,4 @@
 ﻿CREATE TABLE [uni].[programmes] (
 	[programmeID] INT IDENTITY (1,1) PRIMARY KEY,
-	[name] NVARCHAR (255) NOT NULL,
-	[rankScore] INT NULL CHECK(rankScore >=0 AND rankScore <=320) DEFAULT NULL
+	[name] NVARCHAR (255) NOT NULL
 	)
