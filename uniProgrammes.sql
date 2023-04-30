e9693f28f1d4e0f878b625e6728c7b946095c50c
Saaid Faraz <farazsaaid@gmail.com>
2023-05-01 06:38:49 +1200

    Added Rank score field to the uniProgrammes table.

--- uniProgrammes.sql
+++ uniProgrammes.sql
@@ -2,5 +2,6 @@
 	[uniProgrammeID] INT IDENTITY (1,1) PRIMARY KEY,
 	[programmeID] INT NOT NULL REFERENCES [uni].[programmes]([programmeID]),
 	[universityID] INT NOT NULL REFERENCES [uni].[universityInfo]([universityID]),
+	[rankScore] INT NULL CHECK (rankScore <= 320 and rankScore >= 0), 
 	[link] NVARCHAR (255) NOT NULL
 	)
