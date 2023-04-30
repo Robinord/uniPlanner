9f786f6d3cbfb3a85a576f4ca8c403b9bbad1c78
Saaid Faraz <farazsaaid@gmail.com>
2023-05-01 06:43:35 +1200

    Added link field and dropped majorID field from majors Offered table.

--- majorsOffered.sql
+++ majorsOffered.sql
@@ -1,5 +1,5 @@
 ﻿CREATE TABLE [uni].[majorsOffered](
 	[majorOfferedID] INT IDENTITY (1,1) PRIMARY KEY,
 	[uniProgrammeID] INT NOT NULL REFERENCES [uni].[uniProgrammes]([uniProgrammeID]),
-	[majorID] INT NOT NULL REFERENCES [uni].[majors]([majorID]),
+	[link] NVARCHAR(255) NOT NULL,
 )
