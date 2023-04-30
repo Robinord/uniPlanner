99b1ee0a559bef1e24cf45acdd25a97eead9f153
Saaid Faraz <farazsaaid@gmail.com>
2023-05-01 06:43:06 +1200

    Deleted Majors table.

--- majors.sql
+++ majors.sql
@@ -1,5 +1,1 @@
-﻿CREATE TABLE [uni].[majors](
-	[majorID] INT IDENTITY (1,1) PRIMARY KEY,
-	[programmeID] INT NOT NULL REFERENCES [uni].[programmes]([programmeID]),
-	[name] NVARCHAR (255) NOT NULL 
-	)
+﻿
