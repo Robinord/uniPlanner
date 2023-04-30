c6efd5e904e16e1ad74bd1f102ff62ff754d02d2
Saaid Faraz <farazsaaid@gmail.com>
2023-05-01 07:07:57 +1200

    Added name field to majors offered table.

--- majorsOffered.sql
+++ majorsOffered.sql
@@ -1,5 +1,6 @@
 ﻿CREATE TABLE [uni].[majorsOffered](
 	[majorOfferedID] INT IDENTITY (1,1) PRIMARY KEY,
 	[uniProgrammeID] INT NOT NULL REFERENCES [uni].[uniProgrammes]([uniProgrammeID]),
+	[name] NVARCHAR(255) NOT NULL,
 	[link] NVARCHAR(255) NOT NULL,
 )
