a9eb1b91b41a564b447889580a578af460f08c80
Saaid Faraz <farazsaaid@gmail.com>
2023-05-06 11:50:05 +1200

    Changed the placement of fields in uniProgrammes table.

--- uniProgrammes.sql
+++ uniProgrammes.sql
@@ -1,7 +1,7 @@
 ﻿CREATE TABLE [uni].[uniProgrammes] (
 	[uniProgrammeID] INT IDENTITY (1,1) PRIMARY KEY,
-	[programmeID] INT NOT NULL REFERENCES [uni].[programmes]([programmeID]),
 	[universityID] INT NOT NULL REFERENCES [uni].[universityInfo]([universityID]),
+	[programmeID] INT NOT NULL REFERENCES [uni].[programmes]([programmeID]),
 	[rankScore] INT NULL CHECK (rankScore <= 320 and rankScore >= 0), 
 	[link] NVARCHAR (255) NOT NULL
 	)
