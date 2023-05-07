bdb40b65f242036f8222ba24def080b6c1ec5c4a
Saaid Faraz <farazsaaid@gmail.com>
2023-05-07 13:18:19 +1200

    added city and region field to students table.

--- students.sql
+++ students.sql
@@ -2,5 +2,7 @@
 	[email] NVARCHAR(255) PRIMARY KEY,
 	[password] NVARCHAR(255) NOT NULL,
 	[name] NVARCHAR(255),
-	[DOB] date
+	[DOB] date NULL,
+	[city] NVARCHAR (255) NULL,
+	[region] NVARCHAR (255) NULL
 	)
